2c0cc564b620e0913dea034ab2093f4adb14d1e9
Jonathan <contacto@jonathan.com.ar>
2022-12-09 01:23:14 -0300

    Create Replace_With_Alphabet.js

---
+++ 6 kyu/Replace_With_Alphabet.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/546f922b54af40e1e90001da/javascript
+
+const values = {};
+for (let i = 1; i <= 26; i++) {
+  values[String.fromCharCode(96 + i)] = i;
+}
+
+function alphabetPosition(text) {
+  return text
+    .split(" ")
+    .map((a) => a.split(""))
+    .map((a) => a.map((b) => values[b.toLowerCase()]))
+  	.flat()
+  	.filter((a) => a !== undefined)
+    .join(" ")
+    .replace(/,/g, " ")
+}
