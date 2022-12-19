89349b94c649642518d0ee2fbdd27a724f126c6f
Jonathan <contacto@jonathan.com.ar>
2022-12-19 22:14:43 +0000

    Create Quarter_Of_The_Year.js

---
+++ 8 kyu/Quarter_Of_The_Year.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/javascript
+
+const objt = {
+  1: 1,
+  2: 1,
+  3: 1,
+  4: 2,
+  5: 2,
+  6: 2,
+  7: 3,
+  8: 3,
+  9: 3,
+  10: 4,
+  11: 4,
+  12: 4
+}
+
+const quarterOf = (month) => objt[month]
