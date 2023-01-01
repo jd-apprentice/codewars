802a77912f1439c028bf893389105505f649c2c3
Jonathan <contacto@jonathan.com.ar>
2023-01-01 15:51:39 -0300

    Create Apple_Turnover.js

---
+++ 8 kyu/Apple_Turnover.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/580a094553bd9ec5d800007d
+
+function apple(x) {
+  return parseInt(x) * parseInt(x) > 1000
+    ? "It's hotter than the sun!!"
+    : "Help yourself to a honeycomb Yorkie for the glovebox.";
+}
