1044bd990206dcc5c8f7d11b9a54ac284ed9e583
Jonathan <contacto@jonathan.com.ar>
2023-01-06 20:54:34 -0300

    Create Welcome_To_The_City.js

---
+++ 8 kyu/Welcome_To_The_City.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/5302d846be2a9189af0001e4/train/javascript
+
+function sayHello( name, city, state ) {
+  return `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`
+}
