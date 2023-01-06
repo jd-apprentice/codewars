86c1cdb59e1de4e1f80e65872dc65a7b22eeb785
Jonathan <contacto@jonathan.com.ar>
2023-01-06 01:05:23 -0300

    Create Volume_Of_Cuboid.js

---
+++ 8 kyu/Volume_Of_Cuboid.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/58261acb22be6e2ed800003a/train/javascript
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height
+  }
+}
