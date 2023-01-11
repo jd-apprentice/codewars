6c718f46bf00e23ab4cb2d210a65b09967feaa77
Jonathan <contacto@jonathan.com.ar>
2023-01-11 14:38:21 -0300

    Create Flatten_Sort.js

---
+++ 7 kyu/Flatten_Sort.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/57ee99a16c8df7b02d00045f/train/javascript
+
+"use strict";
+
+function flattenAndSort(array) {
+  // Good luck, brave code warrior!
+  return array.flat().sort((a, b) => a - b)
+}
