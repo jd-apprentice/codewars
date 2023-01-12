807d27f070e4fe075fe68f706adbd691f8b82672
Jonathan <contacto@jonathan.com.ar>
2023-01-12 02:33:58 -0300

    Update No_Zeros_For_Heroes.js

--- 8 kyu/No_Zeros_For_Heroes.js
+++ 8 kyu/No_Zeros_For_Heroes.js
@@ -1,3 +1,3 @@
 // https://www.codewars.com/kata/570a6a46455d08ff8d001002/train/javascript
 
-const noBoringZeros = n => n === 0 ? n : Number(n.toString().replace(/0+$/, ''))
+const noBoringZeros = n => 0 || Number(n.toString().replace(/0+$/, ''))
