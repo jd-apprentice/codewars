df85bf178cf0265ab42db684a08cde8c1475c014
Jonathan <contacto@jonathan.com.ar>
2023-01-12 20:09:26 -0300

    Create Predict_Age.js

---
+++ 7 kyu/Predict_Age.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5aff237c578a14752d0035ae/train/javascript
+
+function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
+  return Math.floor(
+    Math.sqrt(
+      [age1, age2, age3, age4, age5, age6, age7, age8]
+        .map((n) => n * n)
+        .reduce((prev, next) => prev + next, 0)
+    ) / 2
+  );
+}
