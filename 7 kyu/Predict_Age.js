9bda396db56270dc8cc4d9fbd1d7acd6c2245220
Jonathan <contacto@jonathan.com.ar>
2023-01-12 20:12:30 -0300

    Update Predict_Age.js

--- 7 kyu/Predict_Age.js
+++ 7 kyu/Predict_Age.js
@@ -3,7 +3,7 @@
 function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
   return Math.floor(
     Math.sqrt(
-      [age1, age2, age3, age4, age5, age6, age7, age8]
+      [...arguments]
         .map((n) => n * n)
         .reduce((prev, next) => prev + next, 0)
     ) / 2
