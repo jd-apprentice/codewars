7145930acdfb5b6ef4e80ec112e1f608ca2be0b9
Jonathan <contacto@jonathan.com.ar>
2023-01-13 01:03:29 -0300

    Create XX_XY.js

---
+++ 8 kyu/XX_XY.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/56530b444e831334c0000020/train/javascript
+
+function chromosomeCheck(sperm) {
+  return sperm.includes("XX") ? "Congratulations! You're going to have a daughter." : "Congratulations! You're going to have a son."
+}
